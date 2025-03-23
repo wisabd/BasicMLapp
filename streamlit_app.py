64e8ff2fa51b0cdf947f19f8732dd4953acee099
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 01:57:44 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,12 @@ st.write('In the area of Buenos Aires, Argentina. A Linear Regression (Machine L
 url = "https://raw.githubusercontent.com/wisabd/BasicMLapp/blob/master/buenos-aires-real-estate-1.csv"
 # Fetch the CSV file content
 response = requests.get(url)
+st.write('In the')
 if response.status_code == 200:
     # Read the CSV content into a DataFrame
+    st.write('In the')
     data = pd.read_csv(StringIO(response.text))
     print(data)
 else:
+    st.write('In the notg;')
     print("Failed to fetch the CSV file.")
