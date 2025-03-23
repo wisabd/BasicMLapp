d636df69b97fb6ae27c1e9106fc64d86284be8f0
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 01:13:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🎈 Real Estate Price Predictor')
 
-st.write('In the area of Buenos Aires, Argentina. A Linear Regression powered app.')
+st.write('In the area of Buenos Aires, Argentina. A Linear Regression (Machine Learning) powered app.')
