cf3f065aeef6c29e90aefcb9458346e6130356e0
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 04:01:10 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ from io import StringIO
     
 st.title('🏢 Real Estate Price Predictor')
 
-
-
 st.write('In the area of Buenos Aires, Argentina. A Linear Regression (Machine Learning) powered app.')
 
 url = "https://raw.githubusercontent.com/wisabd/BasicMLapp/master/buenos-aires-real-estate-1.csv"
@@ -21,9 +19,7 @@ if response.status_code == 200:
         st.write('**Raw Data**')
         data = pd.read_csv(StringIO(response.text))
         data
-
-  
-
+        
     maskcp = data["place_with_parent_names"].str.contains("Capital Federal")
     maskpt = data["property_type"] == "apartment"
     maskpr = data["price"] < 400_000
@@ -32,23 +28,23 @@ if response.status_code == 200:
     low, high = data["surface_covered_in_m2"].quantile([0.1, 0.9])
     mask = data["surface_covered_in_m2"].between(low, high)
     df = data[mask]
-
-    
     
     fig, ax = plt.subplots()
     ax.hist(df['surface_covered_in_m2'], bins=30, color='skyblue', edgecolor='black')
     
-
     ax.set_title("Distribution of Apartment Sizes")
     ax.set_ylabel("Frequency")
     ax.set_xlabel("Area [sq meters]")
 
     st.pyplot(fig)
 
-    chart_data = pd.DataFrame(np.random.randn(1245, 1245), df['surface_covered_in_m2'],df['price_aprox_usd']  )
-    st.scatter_chart(chart_data, x=df['surface_covered_in_m2'], y = df['price_aprox_usd'], color="col4",size="col3")
+    fig, ax = plt.subplots()
+    ax.scatter(df['surface_covered_in_m2'], df['price_aprox_usd'], c='blue', label='Data Points')
+    ax.set_xlabel('Area [Sq metres]')
+    ax.set_ylabel('Price [USD]')
+    ax.set_title('Scatter Plot from DataFrame')
+    st.pyplot(fig)
 
-        
 else:
     st.write('In the notg;')
     print("Failed to fetch the CSV file.")
