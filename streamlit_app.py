63da708ae28768a8848ca6acc14aa7bc9ab96245
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 01:34:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('🏢 Real Estate Price Predictor')
 
 st.write('In the area of Buenos Aires, Argentina. A Linear Regression (Machine Learning) powered app.')
-df = pd.read_csv("https://vm.wqu.edu/lab/tree/work/ds-curriculum/020-housing-in-buenos-aires/data/buenos-aires-real-estate-1.csv")
+df = pd.read_csv("https://github.com/wisabd/BasicMLapp/blob/master/buenos-aires-real-estate-1.csv")
 df
