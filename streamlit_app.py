bf035154315582a28f38b7dd0206d2a32b8bb215
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 01:44:44 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
+
 
 st.title('🏢 Real Estate Price Predictor')
 
+
+
 st.write('In the area of Buenos Aires, Argentina. A Linear Regression (Machine Learning) powered app.')
-df = pd.read_csv("/buenos-aires-real-estate-1.csv")
-df
+
+url = "https://raw.githubusercontent.com/wisabd/BasicMLapp/blob/master/buenos-aires-real-estate-1.csv"
+# Fetch the CSV file content
+response = requests.get(url)
+if response.status_code == 200:
+    # Read the CSV content into a DataFrame
+    data = pd.read_csv(StringIO(response.text))
+    df
+else:
+    print("Failed to fetch the CSV file.")3
