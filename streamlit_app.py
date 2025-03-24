b8cc79f02e2aa6193beb8a71c9695ac42d8b1802
M. Wisal Abdullah <wisal.abdullah.m@gmail.com>
2025-03-24 05:25:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,6 +85,12 @@ if response.status_code == 200:
 
     st.plotly_chart(fig)
 
+    url = "https://raw.githubusercontent.com/wisabd/BasicMLapp/master/buenos-aires-test-features.csv"
+    # Fetch the CSV file content
+    response = requests.get(url)
+    if response.status_code == 200:
+        X_test = pd.read_csv(StringIO(response.text))
+
     mae_baseline = mean_absolute_error(y_train, y_pred_baseline)
     model = LinearRegression()
     X_train = df["surface_covered_in_m2"].values  # Convert to NumPy array
@@ -94,6 +100,44 @@ if response.status_code == 200:
     y_pred_training[:5]
     mae_training = mean_absolute_error(y_train, y_pred_training)
     features = ["surface_covered_in_m2"]
+    X_test = X_test[features]
+    y_pred_test = pd.Series(model.predict(X_test))
+    intercept = round(model.intercept_,2)
+    coefficient = model.coef_[0]
+
+    st.write(f"apt_price = {intercept} + {coefficient} * surface_covered")
+
+
+
+     fig = go.Figure()
+
+    # Add scatter plot for actual data
+    fig.add_trace(
+        go.Scatter(
+        x=X_train,
+        y=y_train,
+        mode='markers',
+        name='Actual Data Points',
+        marker=dict(color='blue')
+    ) ) 
+    fig.add_trace(
+    go.Scatter(
+        x=X_train,
+        y=model.predict(X_train),
+        mode='lines',
+        name='Post-iteration Prediction : Linear Model',
+        line=dict(color='red')
+    ))
+    fig.update_layout(
+    title="Buenos Aires: Price vs. Area",
+    xaxis_title="Area [Sq metres]",
+    yaxis_title="Price [USD]",
+    legend_title="Legend"
+    )
+
+    st.plotly_chart(fig)
+    
+
 
 else:
     st.write('In the notg;')
